9ffbfc83e64f84efd3721013e8d0b9a5b27137aa
suraj65465345 <suraj65465345@gmail.com>
2024-06-03 17:26:02 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "28528007"
-api_hash = "38464da16c80310cabc8d13952419cf3"
-bot_token = "6634064190:AAH8iv5BXtIp_c4ucv3kgcR3MpN25GdSaXA"
+api_id = "24519365"
+api_hash = "48508ad3b574199df57d2a16ae46a4fe"
+bot_token = "7025604490:AAHfDk8NlAJCg9BHJFIyV73DNb1dVIhz-Kk"
